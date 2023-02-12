04023a30ae28465d7832d9ae5875570c3c0967b8
Jakub Jirous <info@jakubjirous.cz>
2023-02-12 15:49:52 -0800

    [pages] not found page arrangement

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,22 @@
+import { Container, Grid } from "@nextui-org/react";
 import React, { FC } from "react";
+import { BackButton } from "../components/Buttons/BackButton";
 import { Page } from "../components/Page/Page";
 
 export const NotFoundPage: FC = () => {
   return (
     <Page>
-      <h1>NotFoundPage</h1>
+      <Container display="flex" justify="center" alignItems="center">
+        <Grid.Container direction="column" alignItems="center" gap={4}>
+          <Grid>
+            <h1>Oops!</h1>
+            <p>The page you're looking for couldn't be found.</p>
+          </Grid>
+          <Grid>
+            <BackButton />
+          </Grid>
+        </Grid.Container>
+      </Container>
     </Page>
   );
 };
