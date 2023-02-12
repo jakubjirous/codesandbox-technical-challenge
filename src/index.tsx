97843ad10bcc6d9c1b4188aebb09c78429ee77b2
Jakub Jirous <info@jakubjirous.cz>
2023-02-12 12:46:48 -0800

    [redux] connect store with app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <Provider store={store}>
+      <h1>Hello World</h1>
+    </Provider>
   </React.StrictMode>
 );
