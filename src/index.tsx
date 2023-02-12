9fecc3fab32558f60a853d90f8b1858a15befc3b
Jakub Jirous <info@jakubjirous.cz>
2023-02-12 13:35:54 -0800

    [styles] connect theme provider with app

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./pages/Router";
 import { store } from "./redux/store";
+import { ThemeProvider } from "./styles/ThemeProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
